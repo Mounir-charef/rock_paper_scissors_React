37b6ab31812c681925ff4243f8819ae15a12d896
Mounir <polomnr1@gmail.com>
2022-12-30 17:41:53 +0100

    added live to Rules.jsx with some animation

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -1,16 +1,15 @@
 import rules from "../images/image-rules.svg"
 function Rules({opacity, onClick}) {
+    const display = opacity? 'active' : ''
     return (
-        opacity ? (
-            <div className={'flex absolute medium:h-full medium:w-full md:p-10 md:gap-5 md:rounded-md md:aspect-square top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 ' +
-            'bg-white flex-col justify-around items-center'}>
+        <div className={'absolute translate-y-full opacity-0 transition-all flex medium:h-full medium:w-full md:p-10 md:gap-5 md:rounded-md md:aspect-square top-1/2 left-1/2' +
+            ' flex-col justify-around items-center bg-white ' + display}>
             <h2 className='text-4xl font-bold'>RULES</h2>
             <img src={rules} alt="rules"/>
             <button className='text-DarkText text-lg font-semibold rounded-md border-2 border-DarkText py-2 px-6 transition hover:bg-DarkText hover:text-white' onClick={onClick}>
                 CLOSE
             </button>
-            </div>
-        ) : null
+        </div>
     );
 }
 export default Rules;
